f1746ea6df613e9fa5f1e89bd6712e0a4f2a632c
paularamayo <a2024110878@estudiantes.upsa.edu.bo>
2024-11-13 13:57:32 -0400

    Create funciones.cpp

---
+++ funciones.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "funciones.h"  // Incluir el archivo de cabecera
+
+using namespace std;
+
+// Definición de la función saludar
+void saludar() {
+    cout << "¡Hola desde el archivo funciones.cpp!" << endl;
+}
+
+// Definición de la función sumar
+int multiplicar(int a, int b) {
+    return a * b;
+}
