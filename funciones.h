781bbcab30ba7f85015a1372e77c2c587b0e85f9
paularamayo <a2024110878@estudiantes.upsa.edu.bo>
2024-11-13 13:58:01 -0400

    Create funciones.h

---
+++ funciones.h
@@ -0,0 +1,9 @@
+#ifndef FUNCIONES_H  // Directiva de inclusión múltiple
+#define FUNCIONES_H
+
+// Declaraciones de funciones
+void saludar();        // Función que imprime un saludo
+int multiplicar(int a, int b); // Función que suma dos números y retorna el resultado
+
+#endif // FUNCIONES_H
+#pragma once
